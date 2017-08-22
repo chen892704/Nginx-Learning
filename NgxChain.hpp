c5b867dc26e938b3a57665e33454efbcf2309b22
ChenXinshuo <892704067@qq.com>
2017-08-22 17:10:37 +0800

    Update NgxChain.hpp

--- NgxChain.hpp
+++ NgxChain.hpp
@@ -11,7 +11,7 @@ class NgxChain final : public NgxWrapper<ngx_chain_t>
 {
 public:
 	typedef NgxWrapper<ngx_chain_t> 	super_type;
-	typedef NgxChain					this_type;
+	typedef NgxChain			this_type;
 	
 public:
 	NgxChain(ngx_chain_t * c) : super_type(c) {}		// 代理链表节点
@@ -19,33 +19,33 @@ public:
 	~NgxChain() = default;
 	
 public:
-	typedef NgxChainIterator	iterator;				// 迭代器类型定义
+	typedef NgxChainIterator	iterator;		// 迭代器类型定义
 	typedef const iterator		const_iterator;
 	
-	iterator begin() const								// 产生迭代器起点
+	iterator begin() const					// 产生迭代器起点
 	{
 		return iterator(get());
 	}
 	
-	iterator end() const								// 产生迭代器终点
+	iterator end() const					// 产生迭代器终点
 	{
 		return iterator();
 	}
 	
-	std::size_t size() const							// 遍历链表，计算总长度
+	std::size_t size() const				// 遍历链表，计算总长度
 	{
 		std::size_t len = 0;
 		
 		for(auto & c : *this)
 		{
-			len += c.data().size();						// 获取节点的 buf 长度
+			len += c.data().size();			// 获取节点的 buf 长度
 		}
 		
 		return len;
 	}
 	
 public:
-	ngx_chain_t * tail() const							// 查找末尾节点
+	ngx_chain_t * tail() const				// 查找末尾节点
 	{
 		auto p = get();
 		
@@ -54,7 +54,7 @@ public:
 		return p;
 	}
 	
-	void append(ngx_chain_t * ch) const					// 向末尾追加节点
+	void append(ngx_chain_t * ch) const			// 向末尾追加节点
 	{
 		tail()->next = ch;
 	}
